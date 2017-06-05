74e5b7ebac381bb5c16f066e4feab5b0cca0f631
Kahumuzab <kahumuza7@gmail.com>
2017-06-05 21:53:14 +0500

    Add files via upload

---
+++ extchr/popup.js
@@ -0,0 +1,10 @@
+function loadUrls(){
+//fetch urls from textarea and load them
+}
+
+function saveUrls(){
+//Fetch urls and save it in chrome storage
+}
+
+document.getElementById('button').addEventListener('click', loadUrls);
+document.getElementById('button').addEventListener('click', saveUrls);
